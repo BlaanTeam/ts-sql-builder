e8b303a01f1d80651f8d74934353167821253a1c
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-01-25 16:41:21 +0100

    refactor: update lib structure
    update:   lib/index.ts
    add:      lib/query-builder/index.ts
    mv:       lib/qb.enums.ts -> lib/query-builder/qb.enums.ts
    mv:       lib/qb.functions.ts -> lib/query-builder/qb.functions.ts
    mv:       lib/qb.interfaces.ts -> lib/query-builder/qb.interfaces.ts
    mv:       lib/qb.operators.ts -> lib/query-builder/qb.operators.ts
    mv:       lib/query-builder.ts -> lib/query-builder/query-builder.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,1 @@
-export * from './query-builder';
-export * from './qb.enums';
-export * from './qb.interfaces';
-export * from './qb.functions';
-export * from './qb.operators';
+export * from './query-builder'
