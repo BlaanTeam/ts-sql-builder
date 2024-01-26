b69e7b0ced9ccb07e12db053cd6922281b424251
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-01-26 16:53:37 +0100

    chore: add jsDoc comments for ColumnMetadata

--- lib/schema-builder/sb.interfaces.ts
+++ lib/schema-builder/sb.interfaces.ts
@@ -11,7 +11,7 @@ export interface TableMetadata {
 export interface IndexMetadata {
   name: string;
   columns: string[];
-  unique?: boolean; // default = false;
+  unique?: boolean; // default false
 }
 
 export interface ForeignKeyMetadata {
@@ -21,13 +21,54 @@ export interface ForeignKeyMetadata {
   onUpdate?: CascadeType;
 }
 
+/**
+ * Define column options
+ */
 export interface ColumnMetadata {
+  /**
+   * Define column name.
+   * @default propertyKey
+   */
   name?: string;
+
+  /**
+   * Define column type in raw sql (depending on the used database)
+   * @example 'INTEGER' 'SERIAL' 'VARCHAR(50)'
+   */
   type: string;
-  nullable?: boolean; // = true
-  unique?: boolean; // = false
+
+  /**
+   * Define nullability
+   * @default true
+   */
+  nullable?: boolean;
+
+  /**
+   * Define uniqueness
+   * @default false
+   */
+  unique?: boolean;
+
+  /**
+   * @optional Define default value for column.
+   */
   default?: string;
+
+  /**
+   * @optional Define check constraint.
+   */
   check?: string;
-  primary?: boolean; // = false
+  
+  /**
+   * Define if this column is (or part of) the primary key.
+   * Same can be achieved when \@PrimaryKey decorator is used.
+   * @default false
+   */
+  primary?: boolean;
+
+  /**
+   * Define foreign key constraint.
+   * Same can be achieved when \@ForeignKey decorator is used.
+   */
   foreignKey?: Omit<ForeignKeyMetadata, 'column'>;
 }
