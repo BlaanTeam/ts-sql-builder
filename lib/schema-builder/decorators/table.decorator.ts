15a1058827b6cb64b5cdeb1e56561107a8be4614
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-01-26 15:23:16 +0100

    chore: update jsDoc comments for table decorator

--- lib/schema-builder/decorators/table.decorator.ts
+++ lib/schema-builder/decorators/table.decorator.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import { TABLE_METADATA_KEY } from '../sb.constants';
 
 /**
- * Decorator for specifying classes that are also tables in the database.
- * Database schema will be generated for all classes decorated with it
- * @param {string} [name] - The name of the table. If not provided, the lowercase class name is used.
- * @returns {ClassDecorator} - The class decorator function.
+ * Decorator to specify a class as a database table.
+ * Database schema will be generated for all classes decorated with it.
+ * 
+ * @param {string} [name] The name of the table. If not provided, the lowercase class name is used.
+ * @returns {ClassDecorator} The class decorator function.
  */
 export function Table(name?: string): ClassDecorator {
   return <T extends Function>(target: T) => {
