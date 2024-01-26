3a1555ab5d6b7a55e62ff68bec477763876454b0
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-01-26 13:29:27 +0100

    chore: create metadata constants

---
+++ lib/schema-builder/sb.constants.ts
@@ -0,0 +1,5 @@
+export const TABLE_METADATA_KEY = 'schema_builder:table_metadata';
+export const COLUMNS_METADATA_KEY = 'schema_builder:table_columns';
+export const PK_METADATA_KEY = 'schema_builder:table_primary_key';
+export const FKS_METADATA_KEY = 'schema_builder:table_foreign_keys';
+export const INDEXES_METADATA_KEY = 'schema_builder:table_indexes';
