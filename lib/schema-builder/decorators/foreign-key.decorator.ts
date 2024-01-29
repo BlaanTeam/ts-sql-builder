bb0d1c7ec1c9bc60fce61a64c4e8b1f886ee55fa
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-01-29 12:50:53 +0100

    chore: add ForeignKey decorator

---
+++ lib/schema-builder/decorators/foreign-key.decorator.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import { ForeignKeyMetadata } from '../sb.interfaces';
+import { FKS_METADATA_KEY } from '../sb.constants';
+
+export function ForeignKey(
+  fkOptions: ForeignKeyMetadata,
+): ClassDecorator & PropertyDecorator {
+  return function (
+    classOrObj: Function | Object,
+    propertyKey?: string | symbol,
+  ) {
+    const target =
+      typeof classOrObj === 'object' ? classOrObj.constructor : classOrObj;
+
+    if (typeof classOrObj === 'function' && !fkOptions.column) {
+      throw new Error(
+        `schema-builder: Property 'column' is required when @ForeignKey is used as a class decorator.`,
+      );
+    }
+
+    fkOptions.column ??= propertyKey?.toString();
+
+    // The following error will never be thrown:
+    // Cause either column was passed, or the decorator was used on a property
+    // which implies 'propertyKey' is defined.
+    if (!fkOptions.column) {
+      throw new Error(`schema-builder: Ambiguous column for ForeignKey.'`);
+    }
+
+    const foreignKeys = Reflect.getMetadata(FKS_METADATA_KEY, target) ?? [];
+
+    foreignKeys.push(fkOptions);
+
+    Reflect.defineMetadata(FKS_METADATA_KEY, foreignKeys, target);
+  };
+}
