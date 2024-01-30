996f17fb66a48b1e7a193d90514c87fe589b1ef7
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-01-30 11:57:55 +0100

    chore: update normalize-value

--- lib/common/normalize-value.ts
+++ lib/common/normalize-value.ts
@@ -4,6 +4,8 @@ export function normalized(value: any): string {
       return `'${value}'`;
     case 'object':
       return `'${JSON.stringify(value)}'`;
+    case 'function':
+      return value() as string;
     default:
       return JSON.stringify(value);
   }
